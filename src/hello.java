6829b187d7dadca30f976a1e2651bf6c9fe04c3a
Praveen Kumar <praveen1898kumar@gmail.com>
2019-03-30 15:00:23 +0530

    Added new printl

--- src/hello.java
+++ src/hello.java
@@ -4,7 +4,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("My first commit");
-		
+		System.out.println("My first change in git");
 
 	}
 
